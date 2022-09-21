58fa25f5cacd30377f1dad76cacea0b6c6a850df
vitoria-ketily <114106299+vitoria-ketily@users.noreply.github.com>
2022-09-21 16:10:52 -0300

    Add files via upload

---
+++ atv.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+    float numero;
+        cin>>numero;
+    if(numero>10){
+        cout<<"Número é maior que 10";
+    }else{
+        cout<<"Número nãso é maior que 10"<<endl;
+    }
+return 0;
+    }
+
+

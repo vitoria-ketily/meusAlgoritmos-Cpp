b3a502156f5c2363945672d9fde4ac51ac08d961
Vitória Ketily <114106299+vitoria-ketily@users.noreply.github.com>
2022-10-21 08:14:30 -0300

    Add files via upload

---
+++ td1Q3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+    int main(){
+        int valor;
+        cout<<"digite o valor:"<<endl;
+        cin>>valor;
+        cout<<"Seu antecessor é:"<<valor-1;
+        cout<<"Seu sucessor é:"<<valor-1;       
+        return 0;
+    }

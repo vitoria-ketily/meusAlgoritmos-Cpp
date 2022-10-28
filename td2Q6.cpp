7c4fc34d9e6f5ee8466fc0a6ce97fe27b9cda580
Vitória Ketily <114106299+vitoria-ketily@users.noreply.github.com>
2022-10-28 08:54:24 -0300

    Update td2Q6.cpp

--- td2Q6.cpp
+++ td2Q6.cpp
@@ -4,9 +4,13 @@ int main (){
     int atual;
     int nascenca;
     cout<<"Digite o ano atual"<<endl;
+    cin>>atual;
+    cout<<"Digite o ano em que nasceu:"<<endl;
     cin>>nascenca;
-    if(atual-nascenca>16){
-        cout<<"não pode votar"<<endl;
+    if(atual-nascenca>=16){
+        cout<<"Você já pode votar."<<endl;
+    }else{
+        cout<<"Você ainda não pode votar."<<endl;
     }
 return 0;
 }

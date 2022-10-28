fa2966b559ad93de2e509273976be25f36787536
Vitória Ketily <114106299+vitoria-ketily@users.noreply.github.com>
2022-10-28 08:29:59 -0300

    Update atv.cpp

--- atv.cpp
+++ atv.cpp
@@ -4,7 +4,7 @@ int main (){
     float numero;
         cin>>numero;
     if(numero>10){
-        cout<<"Número é maior que 10";
+        cout<<"Número é maior que 10"<<endl;
     }else{
         cout<<"Número nãso é maior que 10"<<endl;
     }
